b64253929c05fe2f3bdf16872ea27cd8e1fe2722
Sturba14 <92717011+Sturba14@users.noreply.github.com>
2023-01-08 18:09:08 +0100

    Add files via upload

---
+++ Projektni zadatak 2/news1.php
@@ -0,0 +1,32 @@
+<?php
+print '
+	<main class="news1main">
+        <h1>ASPEN SNOWMASS</h1>
+		<div id="galerija">
+			<figure id="1">
+				    <img src="images/news1galerija1.jpg" alt="Classic Fusion Aerofusion Aspen Snowmass Limited Edition" title="Classic Fusion Aerofusion Aspen Snowmass Limited Edition">
+				    <figcaption>Classic Fusion Aerofusion Aspen Snowmass Limited Edition</figcaption>
+			</figure>
+			<figure id="2">
+				    <img src="images/news1galerija2.jpg" alt="Classic Fusion Aerofusion Aspen Snowmass Limited Edition" title="Classic Fusion Aerofusion Aspen Snowmass Limited Edition">
+				    <figcaption>Classic Fusion Aerofusion Aspen Snowmass Limited Edition (side view)</figcaption>
+			</figure>
+            <figure id="3">
+				    <img src="images/news1galerija3.jpg" alt="Classic Fusion Aerofusion Aspen Snowmass Limited Edition" title="Classic Fusion Aerofusion Aspen Snowmass Limited Edition">
+			    	<figcaption>Classic Fusion Aerofusion Aspen Snowmass Limited Edition (design)</figcaption>
+			</figure>
+		</div>
+		<hr>
+		<div class="news1-1">
+			<p>Aspen Snowmass je enigma. Destinacija prožeta poviješću i zajednicom, također se kreće brzinom i privlači posjetitelje iz cijelog svijeta.</p>
+			<p>Teško je zaustaviti vrijeme u najikoničnijem i najpustolovnijem planinskom gradu u Americi, ali upravo su to danas postigli švicarski luksuzni proizvođač satova Hublot i Aspen Snowmass.</p>
+            <img src="images/news1slika1.jpg" alt="Classic Fusion Aerofusion Aspen Snowmass Limited Edition" title="Classic Fusion Aerofusion Aspen Snowmass Limited Edition">
+			<p>U čast 75. godišnjice Aspen Snowmass-a, Hublot: Službeni mjeritelj vremena Aspen Snowmass-a, započeo je 75. godišnjicu prije vremena, lansiranjem ograničenog izdanja komemorativnog sata.</p>
+			<p>Dizajniran u umjetničkoj suradnji, alpsko-bijeli skeletizirani sat preuzima kultni model Hublot Classic Fusion.</p>
+            <p>Ovo ograničeno izdanje od 30 komada izrađeno od crne keramike s bijelim keramičkim okvirom ima list drveta Aspen na drugoj ruci i sjajni titanij visokog sjaja, što je počast rudarskoj baštini Stjenjaka i grada Aspen.</p>
+            <p>Ograničenje je utisnuto na poleđini sata zajedno s prigodnim logotipom 75 godina: komad je pravi kolekcionarski predmet za 30 sretnika.</p>
+			<p><a href="news.html">Natrag na novosti</a></p>
+		</div>
+	</main>';
+?>
+
---
+++ Projektni zadatak 2/news5.php
@@ -0,0 +1,31 @@
+<?php
+print '
+	<main class="news1main">
+        <h1>NOVAK ĐOKOVIĆ, NOVI HUBLOT AMBASADOR</h1>
+		<div id="galerija">
+			<figure id="1">
+				    <img src="images/news5galerija1.jpg" alt="Novak Đoković" title="Novak Đoković">
+				    <figcaption>Novak Đoković</figcaption>
+			</figure>
+			<figure id="2">
+				    <img src="images/news5galerija2.jpg" alt="Novak Đoković" title="Novak Đoković">
+				    <figcaption>Novak Đoković</figcaption>
+			</figure>
+            <figure id="3">
+				    <img src="images/news5galerija3.jpg" alt="Novak Đoković" title="Novak Đoković">
+			    	<figcaption>Novak Đoković</figcaption>
+			</figure>
+		</div>
+		<hr>
+		<div class="news1-1">
+            <h2><b>Novak Đoković pridružio se obitelji Hublot u društvu legendarnih sportaša kao što su Pelé, Kylian Mbappé, Usain Bolt i Dustin Johnson! Nekoliko dana prije početka US Opena, prvi tenisač svijeta odlučio se postati ambasador švicarskog proizvođača luksuznih satova.</b></h2>
+			<p>Nema potrebe predstavljati Novaka Đokovića – njegov rekord govori sam za sebe. Srpski tenisač osvojio je sve: 85 titula pojedinačno na ATP krugu, uključujući 20 Grand Slamova, 5 Masters i 36 Masters 1000 te olimpijsku medalju.</p>
+			<p>Njegova karijera dovela ga je do vrha ATP ljestvice prije više od deset godina, u srpnju 2011., na poziciji koju i danas drži... nakon 337 tjedana.</p>
+            <img src="images/news5slika1.jpg" alt="Novak Đoković" title="Novak Đoković">
+			<p>Jedini je igrač u Open eri koji je osvojio najmanje dva Grand Slam turnira. A niz je i dalje jak, jer će za nekoliko dana pokušati osvojiti svoj 21. naslov i tako srušiti rekord svih vremena po Grand Slam pobjedama u sljedećem kolu US Opena.</p>
+			<p>Već živa legenda, ali još uvijek na vrhuncu svoje igre, Novak Đoković odlučio se pridružiti velikoj obitelji Hublot ambasadora. Dijeli mnoge sličnosti sa švicarskom tvrtkom za izradu satova Hublot, među kojima je nemilosrdna težnja za izvrsnošću i performansama.</p>
+            <p>Ovo je veliki izvor ponosa za Hublot timove, koji pozdravljaju čovjeka koji se iz dana u dan dokazuje kao najveći tenisač svih vremena. Igra, set i meč za “Nole” i Hublot!</p>
+			<p><a href="news.html">Natrag na novosti</a></p>
+		</div>
+	</main>';
+?>
